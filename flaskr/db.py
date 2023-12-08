fbb3716618490c4aa2caf7158e263255f4e8580e
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2023-12-08 13:01:11 -0800

    fixes typo in app config step 1

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,9 +5,11 @@ from flask import current_app, g
 
 def get_db():
   if 'db' not in g:
-    g.db = sqlite3.connect('/tmp/flaskr.db')
+    g.db = sqlite3.connect(
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
+        )
     g.db.row_factory = sqlite3.Row
-    
+
   return g.db
 
 def close_db(e=None):
